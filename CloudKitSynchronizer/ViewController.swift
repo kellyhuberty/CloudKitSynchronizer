a153a5340c5be55247c2f89b19f768302323a368
Kelly Huberty <kellyhuberty@gmail.com>
2019-03-20 22:14:25 -0500

    Initial Commit

---
+++ CloudKitSynchronizer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CloudKitSynchronizer
+//
+//  Created by Kelly Huberty on 3/20/19.
+//  Copyright © 2019 Kelly Huberty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
