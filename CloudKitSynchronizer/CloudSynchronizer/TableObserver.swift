a09d7b477803baa40e03e3a35453b506a39e8bbd
Kelly Huberty <kellyhuberty@gmail.com>
2019-07-06 10:59:09 -0500

    Half way working, null value constraint failing.

---
+++ CloudKitSynchronizer/CloudSynchronizer/TableObserver.swift
@@ -0,0 +1,9 @@
+//
+//  TableObserver.swift
+//  CloudKitSynchronizer
+//
+//  Created by Kelly Huberty on 5/11/19.
+//  Copyright © 2019 Kelly Huberty. All rights reserved.
+//
+
+import Foundation
