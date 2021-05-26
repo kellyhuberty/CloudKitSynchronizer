fde26f6f337985c1c7c67cec34e49a6872bec113
Kelly Huberty <kellyhuberty@gmail.com>
2021-05-25 21:41:10 -0500

    Additional tests for weak array

--- CloudKitSynchronizerTests/WeakArrayTests.swift
+++ CloudKitSynchronizerTests/WeakArrayTests.swift
@@ -11,11 +11,24 @@ import XCTest
 
 class WeakArrayTests: XCTestCase {
 
-    var subject: WeakArray<NSObject>!
+    var subject: WeakArray<TestObject>!
+    
+    class TestObject: Identifiable, Equatable {
+    
+        let id: String
+        
+        init() {
+            id = UUID().uuidString
+        }
+        
+        static func == (lhs: WeakArrayTests.TestObject, rhs: WeakArrayTests.TestObject) -> Bool {
+            return lhs.id == rhs.id
+        }
+    }
     
     override func setUpWithError() throws {
         
-        subject = WeakArray<NSObject>()
+        subject = WeakArray<TestObject>()
         
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -28,22 +41,60 @@ class WeakArrayTests: XCTestCase {
 
     func testAddObject() throws {
         
-        var indexPath: NSObject? = NSObject()
-        subject.append(indexPath!)
+        var object: TestObject? = TestObject()
+        subject.append(object!)
         
         XCTAssertEqual(subject.count, 1)
-        indexPath = nil
+        object = nil
         XCTAssertEqual(subject.count, 0)
     }
 
     func testRemoveObject() throws {
         
-        var indexPath: NSObject? = NSObject()
-        subject.append(indexPath!)
+        var object: TestObject? = TestObject()
+        subject.append(object!)
         
         XCTAssertEqual(subject.count, 1)
         subject.removeAll()
         XCTAssertEqual(subject.count, 0)
+    }
+    
+    func testAddObjects() throws {
+        
+        let object1: TestObject = TestObject()
+        var object2: TestObject = TestObject()
+        let object3: TestObject = TestObject()
+
+        subject.append(object1)
+        subject.append(object2)
+        subject.append(object3)
+
+        XCTAssertEqual(subject.count, 3)
+        
+        XCTAssert(subject[0] == object1)
+        XCTAssert(subject[1] == object2)
+        XCTAssert(subject[2] == object3)
+
+    }
+
+    func testRemoveObjects() throws {
+        
+        let object1: TestObject = TestObject()
+        var object2: TestObject? = TestObject()
+        let object3: TestObject = TestObject()
+
+        subject.append(object1)
+        subject.append(object2!)
+        subject.append(object3)
+
+        
+        XCTAssertEqual(subject.count, 3)
+        object2 = nil
+        XCTAssertEqual(subject.count, 2)
+        
+        XCTAssert(subject.contains(object1))
+        XCTAssert(subject.contains(object3))
+
     }
     
     func testPerformanceExample() throws {
